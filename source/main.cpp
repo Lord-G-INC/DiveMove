cdcb63b83e83ee93c3ab71d90ce85b01fc12dd56
nico-apps <nicomarikucza@googlemail.com>
2024-06-02 02:32:48 +0200

    Use Common Function Hook module

--- source/main.cpp
+++ source/main.cpp
@@ -4,12 +4,11 @@ bool isDive = false;
 
 void checkIfDiveAllowed () {
     if (!MR::isOnGroundPlayer() && !isDive && MR::testSubPadTriggerC(0)) {
-        MarioAccess::getPlayerActor()->mMario->trySquatJump();
         isDive = true;
+        MarioAccess::getPlayerActor()->mMario->trySquatJump();
     } else if (MR::isOnGroundPlayer() && isDive) 
         isDive = false; // Reset Dive
 }
-kmBranch(0x80388DA0, checkIfDiveAllowed);
 
 // Change Camera Reset Button to D-Pad Down
 kmWrite32(0x8011E058, 0x4BF0C319); // bl MR::testCorePadTriggerDown()
