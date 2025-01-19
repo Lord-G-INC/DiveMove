9ec28b4d3da94783a86a7bb64313107ddf90d091
VTXG <VTXG@users.noreply.github.com>
2025-01-19 22:12:22 +0000

    Back to the A button

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,28 @@
 #include "syati.h"
 #include "kamek/hooks.h"
-bool isDive = false;
+
+#define DIVE_STATE_DISABLED 0
+#define DIVE_STATE_DELAY 1
+#define DIVE_STATE_IN_DIVE 2
+
+u32 diveState = DIVE_STATE_DISABLED;
 
 void checkIfDiveAllowed () {
-    if (!MR::isOnGroundPlayer() && !isDive && MR::testSubPadTriggerC(0)) {
-        isDive = true;
-        MarioAccess::getPlayerActor()->mMario->trySquatJump();
-    } else if (MR::isOnGroundPlayer() && isDive) 
-        isDive = false; // Reset Dive
+    Mario *mMario = MarioAccess::getPlayerActor()->mMario;
+
+    if (!MR::isOnGroundPlayer() && !mMario->isStatusActive(MARIO_WALL)) {
+        if (diveState == DIVE_STATE_IN_DIVE || !MR::testCorePadTriggerA(0))
+            return;
+
+        if (diveState == DIVE_STATE_DELAY) {
+            MarioAccess::getPlayerActor()->mMario->trySquatJump();
+            diveState = DIVE_STATE_IN_DIVE;
+        }
+        else
+            diveState = DIVE_STATE_DELAY;
+    }
+    else
+        diveState = DIVE_STATE_DISABLED;
 }
 
 // Change Camera Reset Button to D-Pad Down
